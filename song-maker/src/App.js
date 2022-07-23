393f8636153bcdd1a8e2bc99e5f75b37c0e41cd3
jina <jk020218@gmail.com>
2022-07-24 03:18:38 +0900

    seperated oneClick and multipleClick

--- song-maker/src/App.js
+++ song-maker/src/App.js
@@ -1,13 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react' //useSate import
 import SongMakerSelf from './SongMakerSelf/SongMakerSelf';
 
 function App() {
+  const [inputValue,setInputValue]=useState(1)
+
+  function getNoteLength(){
+  setInputValue(inputValue);
+  }
+
   return (
     <div className="App">
-      <SongMakerSelf column={10}/>
+      <h1>HeadBar!!!</h1>
+        <input type="number" onChange={(e)=>setInputValue(e.target.valueAsNumber)}></input>
+        <button onClick={getNoteLength}>입력</button>
+        <NoteLength inputValue={inputValue} />
+
+      <SongMakerSelf column={10} noteLength={inputValue}/>
     </div>
   );
+}
+
+function NoteLength(props){
+  return(
+      <h2>note 길이는 {props.inputValue}!!!!</h2>
+  )
 }
 
 export default App;
--- song-maker/src/SongMakerSelf/SongMakerSelf.js
+++ song-maker/src/SongMakerSelf/SongMakerSelf.js
@@ -1,6 +1,13 @@
 import React, {useState, useEffect} from 'react'
 import './SongMakerSelf.css'
-const SongMakerSelf = (props) => {
+
+
+ //props로 column 수를 받아옴-> 업데이트 가능하게 추후 구현 필요*********
+ //지금 짝수번 클릭시에만 색깔 칠해지게 구현됨***********
+ //지금은 button 상관없이 입력하기만 하면 바로바로 업데이트 되고 있음*********
+//10으로 적힌 부분들 props.column으로 바꿔주기
+
+ const SongMakerSelf = (props) => {
     // const note=["C5, "]
     const colors=[
     "#E33059",
@@ -16,14 +23,30 @@ const SongMakerSelf = (props) => {
     "#A347BF",
     "#EA57B2",
 ];
-    const [background, setBackground]=useState("#FFFFFFF")
-    const [cells, setCells]=useState(Array(25*props.column).fill(''));
+    const [background, setBackground]=useState("#FFFFFF");
+    const [border, setBorder]=useState("#000000")
+    const [cells, setCells]=useState(Array(25*props.column).fill('#FFFFFF'));
+    const [cellsBorder, setCellsBorder]=useState(Array(25*props.column).fill("#000000"));
+    
+    const noteLength=props.noteLength;
+    let noteLengthIsOne=true;
+    let squares=[...cells];
+    let squaresBorder=[...cellsBorder];
 
-    //props로 column 수를 받아옴-> 업데이트 가능하게 추후 구현 필요
-    const makeColumnfunction= (j)=> {
+    console.log("Init: squares : ", squares)
+    console.log("Init: borders: ", squaresBorder)
+
+    //noteLength 1 여부 확인하기
+    console.log(noteLength)
+    
+    if (noteLength==1)noteLengthIsOne=true;
+    else noteLengthIsOne=false;
+
+    //table column, row 그려주기
+    const makeColumn= (j)=> {
         const column = [];
         for (let i = 0; i < props.column; i++) {
-            column.push(<Cell num={10*j+i}/>);
+            column.push(<Cell num={10*j+i} noteLength={noteLength}/>);
         };
         return column;
     };
@@ -32,40 +55,100 @@ const SongMakerSelf = (props) => {
         for (let j=24;j>=0;j--){
             row.push(<tr>
                 <h3 className='note_name'>C</h3>
-                {makeColumnfunction(j)}
+                {makeColumn(j)}
             </tr>);
         }
         return row;
     };
 
+    //note 길이 1일때 : 각 Cell에 ClickListener 설정하기
     const handleClick=(num)=>{
-        let squares=[...cells];
-        if(background=="#FFFFFFF"){
+        if(squares[num]=="#FFFFFF"){//note 추가
             squares[num]=colors[parseInt(num/10)%12]
             setBackground(squares[num])
-        }else{
-            squares[num]="#FFFFFFF"
-            setBackground("#FFFFFFF")
+        }else{//note 제거
+            squares[num]="#FFFFFF"
+            setBackground(squares[num])
         }
-        // keyClassNAme=" clicked"
+        squaresBorder[num]="#000000"
+        console.log("Click: squares : ", squares)
+        console.log("NoClick: borders: ", squaresBorder)
+
         setCells(squares)
+        setCellsBorder(squaresBorder)
+        // alert(num);
+    }
+    //note 길이 2 이상일 때
+    const handleLongClick=(num, noteLength)=>{
+        //background 설정
+        if(squares[num]=="#FFFFFF"){//note 추가
+            console.log("if에 있어!!!!")
+            let newColor=colors[parseInt(num/10)%12]
+            squares[num]=newColor
+            squaresBorder[num]=newColor
+            setBackground(squares[num])
+            setBorder(squaresBorder[num])
+
+            // for(let i=0;i<noteLength;i++){
+            //     squares[num+i]=newColor
+            //     squaresBorder[num+i]=newColor
+            //     setBackground(squares[num+i])
+            //     setBorder(squaresBorder[num+i])
+            // }
+            // setBackground(squares[num])
+            // setBorder(squaresBorder[num])
+            console.log("longNewClick: squares : ", squares)
+            console.log("longNewClick: borders: ", squaresBorder)
+
+        }else{//note 제거
+            console.log("else에 있어!!!!")
+            // for(let i=0;i<noteLength;i++){
+            //     squares[num+i]="#FFFFFF"
+            //     squaresBorder[num+i]="#000000"
+            //     setBackground(squares[num+i])
+            //     setBorder(squaresBorder[num+i])
+            // }
+            // setBackground(squares[num])
+            // setBorder(squaresBorder[num])
+            console.log("longNoClick: squares : ", squares)
+            console.log("longNoClick: borders: ", squaresBorder)
+            squares[num]="#FFFFFF"
+            squaresBorder[num]="#000000"
+            setBackground(squares[num])
+            setBorder(squaresBorder[num])
+        }
+        setCells(squares)
+        setCellsBorder(squaresBorder)
         // alert(num);
     }
 
-    const Cell=({num})=>{
+    const Cell=({num}, {noteLength})=>{
+        if (noteLengthIsOne){//note 길이가 1일 경우
         return <td 
         style={{
-            background: cells[num], 
+            background: cells[num],
+            border:'1px solid '+cellsBorder[num]
         }}
         onClick={()=>
             handleClick(num)}></td>
     }
+    else{//note 길이가 1보다 길 경우
+        return <td 
+        style={{
+            background: cells[num],
+            border:'1px solid '+cellsBorder[num]
+        }}
+        onClick={()=>
+            handleLongClick(num, noteLength)}></td>
+    }
+
+}
+    
+
+
   return (
     <div className='container' style={{background:"#FFFFFF"}}>
-        <h1>HeadBar!!!</h1>
-        <h1>Bar!!!</h1>
-        
-      <table>
+      <table cellpadding="0" cellspacing="0">
         <tbody>
             {makeRow()}
         </tbody>
@@ -75,5 +158,6 @@ const SongMakerSelf = (props) => {
     </div>
   )
 }
+
 
 export default SongMakerSelf
