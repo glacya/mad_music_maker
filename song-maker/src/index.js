f506bb06550ec46dcf18084de5c9ea0eab686f77
gcorinee <ori0309@naver.com>
2022-07-27 05:04:10 +0900

    22

--- song-maker/src/index.js
+++ song-maker/src/index.js
@@ -2,11 +2,12 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import Show from "./score/show"
 import Test from "./test"
+import AppSongMaker from "./SongMakerSelf/AppSongMaker"
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
-    <Show />
+    <AppSongMaker />
   </StrictMode>,
   rootElement
 );
