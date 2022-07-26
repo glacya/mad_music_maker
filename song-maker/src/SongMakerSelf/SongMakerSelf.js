7dc3beea6a6c280be05338da91f8e29beca73f2f
jina <jk020218@gmail.com>
2022-07-27 03:29:58 +0900

    total length 수정 필요

--- song-maker/src/SongMakerSelf/SongMakerSelf.js
+++ song-maker/src/SongMakerSelf/SongMakerSelf.js
@@ -1,3 +1,4 @@
+import  axios  from 'axios';
 import React, {useState, useEffect} from 'react'
 import './SongMakerSelf.css'
 
@@ -8,6 +9,33 @@ import './SongMakerSelf.css'
 //10으로 적힌 부분들 props.column으로 바꿔주기
 
  const SongMakerSelf = (props) => {
+    console.log("nodeLength: "+props.noteLength)
+    console.log("node temp: "+props.tempo)
+    console.log("note_type: "+props.note_type)
+    console.log("rhythm: "+props.rhythm)
+
+    const tempo=props.tempo;
+    // let total_length=0;///수정하기*************
+    const [total_length, setTotalLength]=useState(0);
+    // let number_of_notes=0;
+    const [number_of_notes, setNumberOfNotes]=useState(0);
+    const rhythm=props.rhythm;
+    let note_type=props.note_type ////input 받아줘야!!!!!
+    // let notes=[] //클래스 생성 필요*************** ->noteClass
+    // const [newNotes, setNewNotes]=useState({start: 0, length: 0, pitch:0, note_type: {note_type}});//{"start": 0, "length": 0, "pitch":0, "noteType": {note_type}}
+    const [notes, setNotes]=useState([]);//이렇게 초기화하는 게 맞는 건지..****
+    // let [inputValue,setInputValue]=useState('');
+    
+
+    // class noteClass{
+    //     constructor(start, length, pitch, note_type){
+    //         this.start=start;
+    //         this.length=length;
+    //         this.pitch=pitch;
+    //         this.note_type=note_type;
+    //     }
+    // }
+    
     // const note=["C5, "]
     const colors=[
     "#E33059",
@@ -33,13 +61,13 @@ import './SongMakerSelf.css'
     let squares=[...cells];
     let squaresBorder=[...cellsBorder];
 
-    console.log("Init: squares : ", squares)
-    console.log("Init: borders: ", squaresBorder)
+    // console.log("Init: squares : ", squares)
+    // console.log("Init: borders: ", squaresBorder)
 
     //noteLength 1 여부 확인하기
-    console.log(noteLength)
+    console.log("noteLength : "+noteLength)
     
-    if (noteLength==1)noteLengthIsOne=true;
+    if (noteLength===1)noteLengthIsOne=true;
     else noteLengthIsOne=false;
 
     //table column, row 그려주기
@@ -63,7 +91,47 @@ import './SongMakerSelf.css'
 
     //note 길이 1일때 : 각 Cell에 ClickListener 설정하기
     const handleClick=(num)=>{
-        if(squares[num]=="#FFFFFF"){//note 추가
+        console.log("note 길이 1인 handleClick!!!!!")
+        setNumberOfNotes(prevNumber=>prevNumber+1)
+        // number_of_notes+=1;
+        console.log("HANDLECLICK", number_of_notes);
+        // total_length+=1;
+        
+        // const {start,length, pitch, note_type }=newNotes
+
+        // setNewNotes({///이게 맞는 건지..********
+        //     ...newNotes,
+        //     start: num%props.column,
+        //     length: 1,
+        //     pitch:parseInt(num/(props.column)),
+        //     note_type: note_type
+        // });
+        
+        const notee={//id 추가해야??
+            start: num%props.column,
+            length: 1,
+            pitch:parseInt(num/(props.column)),
+            note_type: note_type
+        };
+        console.log("notee : ",notee)
+        // setNotes([...notes, notee]);
+        setNotes(notes.concat(notee));
+        
+        console.log("###############################################")
+        // console.log("notes 배열!!!!"+JSON.stringify(notes[1]))
+        console.log("notes concat 배열 : ",notes.concat(notee));
+        // setNewNotes({
+        //     "start": 0,
+        //     "length": 0, 
+        //     "pitch":0, 
+        //     "noteType": {note_type}
+        // });
+
+        //************* */
+        // let creatNoteClass=noteClass(num%props.column, 1, parseInt(num/(props.column)),note_type);
+        // notes.add
+
+        if(squares[num]==="#FFFFFF"){//note 추가
             squares[num]=colors[parseInt(num/10)%12]
             setBackground(squares[num])
         }else{//note 제거
@@ -71,8 +139,8 @@ import './SongMakerSelf.css'
             setBackground(squares[num])
         }
         squaresBorder[num]="#000000"
-        console.log("Click: squares : ", squares)
-        console.log("NoClick: borders: ", squaresBorder)
+        // console.log("Click: squares : ", squares)
+        // console.log("NoClick: borders: ", squaresBorder)
 
         setCells(squares)
         setCellsBorder(squaresBorder)
@@ -80,9 +148,30 @@ import './SongMakerSelf.css'
     }
     //note 길이 2 이상일 때
     const handleLongClick=(num, noteLength)=>{
+        console.log("note 길이 2 이상인 handleLongClick")
+        setNumberOfNotes(prevNumber=>prevNumber+1)
+        console.log("HANDLECLICK", number_of_notes);
+
+        const notee={//id 추가해야??
+            start: num%props.column,
+            length: noteLength,
+            pitch:parseInt(num/(props.column)),
+            note_type: note_type
+        };
+        console.log("notee : ",notee)
+        // setNotes([...notes, notee]);
+        setNotes(notes.concat(notee));
+        
+        console.log("###############################################")
+        // console.log("notes 배열!!!!"+JSON.stringify(notes[1]))
+        console.log("notes concat 배열 : ",notes.concat(notee));
+
+        // number_of_notes+=1;
+        // setNumberOfNotes(prevNumber=>prevNumber+1)
+        // total_length+=noteLength;
         //background 설정
-        if(squares[num]=="#FFFFFF"){//note 추가
-            console.log("if에 있어!!!!")
+        if(squares[num]==="#FFFFFF"){//note 추가
+            console.log("if에 있어!!!!note 추가")
             let newColor=colors[parseInt(num/10)%12]
             squares[num]=newColor
             squaresBorder[num]=newColor
@@ -97,11 +186,11 @@ import './SongMakerSelf.css'
             // }
             // setBackground(squares[num])
             // setBorder(squaresBorder[num])
-            console.log("longNewClick: squares : ", squares)
-            console.log("longNewClick: borders: ", squaresBorder)
+            // console.log("longNewClick: squares : ", squares)
+            // console.log("longNewClick: borders: ", squaresBorder)
 
         }else{//note 제거
-            console.log("else에 있어!!!!")
+            console.log("else에 있어!!!!note 제거")
             // for(let i=0;i<noteLength;i++){
             //     squares[num+i]="#FFFFFF"
             //     squaresBorder[num+i]="#000000"
@@ -110,8 +199,8 @@ import './SongMakerSelf.css'
             // }
             // setBackground(squares[num])
             // setBorder(squaresBorder[num])
-            console.log("longNoClick: squares : ", squares)
-            console.log("longNoClick: borders: ", squaresBorder)
+            // console.log("longNoClick: squares : ", squares)
+            // console.log("longNoClick: borders: ", squaresBorder)
             squares[num]="#FFFFFF"
             squaresBorder[num]="#000000"
             setBackground(squares[num])
@@ -141,8 +230,117 @@ import './SongMakerSelf.css'
         onClick={()=>
             handleLongClick(num, noteLength)}></td>
     }
-
 }
+    const sortArray=()=>{
+        console.log("$$$$$$$$$$$$$$$$$$$$$$$$$4")
+        console.log("node num:",number_of_notes)
+        console.log(notes)
+        //notes 배열 정렬
+        const startAscending=[...notes].sort((a,b)=>a.start-b.start);
+        console.log("startAscending:  ",startAscending) ///확인하기!!!!!//pitch까지는 구현 못함...
+
+        //total_length
+        console.log("total_length!!!: ",startAscending[number_of_notes-1].start+startAscending[number_of_notes-1].length-1-startAscending[0].start+1);
+        // total_length=startAscending[number_of_notes-1].start+startAscending[number_of_notes-1].length-1-startAscending[0].start+1;
+        setTotalLength(startAscending[number_of_notes-1].start+startAscending[number_of_notes-1].length-1-startAscending[0].start+1);
+        console.log("TotalLength: ",total_length);
+
+        return startAscending
+    }
+    async function postData(e){
+        e.preventDefault();
+
+        const startAscending=sortArray();
+        
+        console.log("TotalLength: ",total_length);
+        axios.post("/api/audio_playtest",JSON.stringify({
+            tempo: tempo,
+            total_length: total_length,
+            number_of_notes: number_of_notes,
+            rhythm:rhythm,
+            notes:startAscending
+        })
+        , {
+            headers: {"Content-Type": 'application/json'},
+        }).then((res)=>{
+            console.log("RES")
+            console.log(res)
+        })
+    
+    }
+    // async function postData(e){
+    //     e.preventDefault();
+    //     try{
+    //         const response=await axios.post("http://localhost:3000/api/audio_playtest",JSON.stringify({sdf:"33"})
+    //         , {
+    //             headers: {"Content-Type": 'application/json'}
+    //         });
+    //         console.log(response)
+    //     }catch(error){
+    //         console.error(error);
+    //     }
+    // }
+
+    // tempo: 12,
+    // total_length: 12,
+    // number_of_notes: number_of_notes,
+    // rhythm:rhythm,
+    // notes:[{start: 5, length: 1, pitch: 0, note_type: 'saw'}]
+    
+    // const postData=async(e)=>{
+    //     //page reload 방지
+    //     e.preventDefault();
+
+        // axios({
+        //     method: "post",
+        //     url: "http://192.249.18.201:443/audio_playtest",
+        //     responseType: "text",
+            // data:{
+            //         tempo: 12,
+            //         total_length: 12,
+            //         number_of_notes: number_of_notes,
+            //         rhythm:rhythm,
+            //         notes:[{start: 5, length: 1, pitch: 0, note_type: 'saw'}]
+            //     }
+        // }).then(function (response) {
+        //     // response Action
+            
+        //     console.log(response)
+        // });
+        
+
+
+        // try{
+        //     const response=await axios.post('http://192.249.18.201:443/audio_playtest',JSON.stringify({
+        //     tempo: 12,
+        //     total_length: 12,
+        //     number_of_notes: number_of_notes,
+        //     rhythm:rhythm,
+        //     notes:[{start: 5, length: 1, pitch: 0, note_type: 'saw'}]
+        // }),{
+        //     headers: {
+        //         "Access-Control-Allow-Origin": ["http://localhost:3000", "http://192.249.18.201:443"]
+        //     }
+        // });
+        // } catch(err){
+        //     if(!err?.response){
+        //         console.log(err)
+        //     }
+        // }
+
+        // axios.post('http://192.249.18.201:443/audio_playtest',JSON.stringify({
+        //     tempo: 12,
+        //     total_length: 12,
+        //     number_of_notes: number_of_notes,
+        //     rhythm:rhythm,
+        //     notes:[{start: 5, length: 1, pitch: 0, note_type: 'saw'}]
+        // }),{
+        //     headers: {
+        //         "Access-Control-Allow-Origin": ["http://localhost:3000", "http://192.249.18.201:443"]
+        //     },
+        // }).then(res=>console.log('Posting Data', res))
+        // .catch(err=>console.log(err))
+    // }   
     
 
 
@@ -153,6 +351,7 @@ import './SongMakerSelf.css'
             {makeRow()}
         </tbody>
       </table>
+      <button onClick={postData}>Test Sound</button>
       <h1>ToolBar!!!</h1>
       <h1>End!!!!</h1>
     </div>
